8194eef5a90c85773191427b166be4a3ae92e59c
Filip Dujmusic <filip@ampnet.io>
2022-08-16 19:03:35 +0200

    update PaymentSplitter to support solc@0.8.0

--- contracts/dev3/paymentsplitter/PaymentSplitter.sol
+++ contracts/dev3/paymentsplitter/PaymentSplitter.sol
@@ -1,70 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.4.24;
-
-/**
- * @title SafeMath
- * @dev Math operations with safety checks that revert on error
- */
-library SafeMath {
-
-  /**
-  * @dev Multiplies two numbers, reverts on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-    // benefit is lost if 'b' is also tested.
-    // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-    if (a == 0) {
-      return 0;
-    }
-
-    uint256 c = a * b;
-    require(c / a == b);
-
-    return c;
-  }
-
-  /**
-  * @dev Integer division of two numbers truncating the quotient, reverts on division by zero.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b > 0); // Solidity only automatically asserts when dividing by 0
-    uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-    return c;
-  }
-
-  /**
-  * @dev Subtracts two numbers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b <= a);
-    uint256 c = a - b;
-
-    return c;
-  }
-
-  /**
-  * @dev Adds two numbers, reverts on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-    uint256 c = a + b;
-    require(c >= a);
-
-    return c;
-  }
-
-  /**
-  * @dev Divides two numbers and returns the remainder (unsigned integer modulo),
-  * reverts when dividing by zero.
-  */
-  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-    require(b != 0);
-    return a % b;
-  }
-}
-
+pragma solidity ^0.8.0;
 
 /**
  * @title ERC20 interface
@@ -99,84 +34,12 @@ interface IERC20 {
   );
 }
 
-
-/**
- * @title SafeERC20
- * @dev Wrappers around ERC20 operations that throw on failure.
- * To use this library you can add a `using SafeERC20 for ERC20;` statement to your contract,
- * which allows you to call the safe operations as `token.safeTransfer(...)`, etc.
- */
-library SafeERC20 {
-
-  using SafeMath for uint256;
-
-  function safeTransfer(
-    IERC20 token,
-    address to,
-    uint256 value
-  )
-    internal
-  {
-    require(token.transfer(to, value));
-  }
-
-  function safeTransferFrom(
-    IERC20 token,
-    address from,
-    address to,
-    uint256 value
-  )
-    internal
-  {
-    require(token.transferFrom(from, to, value));
-  }
-
-  function safeApprove(
-    IERC20 token,
-    address spender,
-    uint256 value
-  )
-    internal
-  {
-    // safeApprove should only be called when setting an initial allowance, 
-    // or when resetting it to zero. To increase and decrease it, use 
-    // 'safeIncreaseAllowance' and 'safeDecreaseAllowance'
-    require((value == 0) || (token.allowance(msg.sender, spender) == 0));
-    require(token.approve(spender, value));
-  }
-
-  function safeIncreaseAllowance(
-    IERC20 token,
-    address spender,
-    uint256 value
-  )
-    internal
-  {
-    uint256 newAllowance = token.allowance(address(this), spender).add(value);
-    require(token.approve(spender, newAllowance));
-  }
-
-  function safeDecreaseAllowance(
-    IERC20 token,
-    address spender,
-    uint256 value
-  )
-    internal
-  {
-    uint256 newAllowance = token.allowance(address(this), spender).sub(value);
-    require(token.approve(spender, newAllowance));
-  }
-}
-
-
 /**
  * @title PaymentSplitter
  * @dev This contract can be used when payments need to be received by a group
  * of people and split proportionately to some number of shares they own.
  */
 contract PaymentSplitter {
-  using SafeMath for uint256;
-  using SafeERC20 for IERC20;
 
   event PayeeAdded(address account, uint256 shares);
   event PaymentReleased(address to, address token, uint256 amount);
@@ -191,12 +54,12 @@ contract PaymentSplitter {
   /**
    * @dev Constructor
    */
-  constructor(address[] payees, uint256[] shares) public {
-    require(payees.length == shares.length);
-    require(payees.length > 0);
+  constructor(address[] memory payeesList, uint256[] memory sharesList) {
+    require(payeesList.length == sharesList.length);
+    require(payeesList.length > 0);
 
-    for (uint256 i = 0; i < payees.length; i++) {
-      _addPayee(payees[i], shares[i]);
+    for (uint256 i = 0; i < payeesList.length; i++) {
+      _addPayee(payeesList[i], sharesList[i]);
     }
   }
 
@@ -243,19 +106,15 @@ contract PaymentSplitter {
   function release(address token, address account) external {
     require(_shares[account] > 0);
 
-    uint256 totalReceived = IERC20(token).balanceOf(address(this)).add(_totalReleased[token]);
-    uint256 payment = totalReceived.mul(
-      _shares[account]).div(
-        _totalShares).sub(
-          _released[token][account]
-    );
+    uint256 totalReceived = IERC20(token).balanceOf(address(this)) + _totalReleased[token];
+    uint256 payment = ((totalReceived * _shares[account]) / _totalShares) - _released[token][account];
 
     require(payment != 0);
 
-    _released[token][account] = _released[token][account].add(payment);
-    _totalReleased[token] = _totalReleased[token].add(payment);
+    _released[token][account] = _released[token][account] + payment;
+    _totalReleased[token] = _totalReleased[token] + payment;
 
-    IERC20(token).safeTransfer(account, payment);
+    IERC20(token).transfer(account, payment);
     emit PaymentReleased(account, token, payment);
   }
 
@@ -271,7 +130,7 @@ contract PaymentSplitter {
 
     _payees.push(account);
     _shares[account] = shares_;
-    _totalShares = _totalShares.add(shares_);
+    _totalShares = _totalShares + shares_;
     emit PayeeAdded(account, shares_);
   }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,9 +7,6 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.4.24"
-      },
-      {
         version: "0.8.0"
       }
     ]

10e8dd878b227328bb02dc7b9f187e2ac8bf3761
Filip Dujmusic <filip@ampnet.io>
2022-10-25 17:40:48 +0200

    add erc721receivable on nft rewarder implementation

--- contracts/dev3/nft-rewarder/NftRewarder.sol
+++ contracts/dev3/nft-rewarder/NftRewarder.sol
@@ -262,9 +262,33 @@ interface IERC721 is IERC165 {
 }
 
 
+/**
+ * @title ERC721 token receiver interface
+ * @dev Interface for any contract that wants to support safeTransfers
+ * from ERC721 asset contracts.
+ */
+interface IERC721Receiver {
+    /**
+     * @dev Whenever an {IERC721} `tokenId` token is transferred to this contract via {IERC721-safeTransferFrom}
+     * by `operator` from `from`, this function is called.
+     *
+     * It must return its Solidity selector to confirm the token transfer.
+     * If any other value is returned or the interface is not implemented by the recipient, the transfer will be reverted.
+     *
+     * The selector can be obtained in Solidity with `IERC721Receiver.onERC721Received.selector`.
+     */
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+}
+
+
 // File contracts/managers/nft-rewarder/NftRewarder.sol
 
-contract NftRewarder is Ownable {
+contract NftRewarder is Ownable, IERC721Receiver {
 
     event AddReward(bytes32 secretHash);
     event ClaimReward(address wallet, bytes32 secretHash);
@@ -326,5 +350,14 @@ contract NftRewarder is Ownable {
         return keccak256(
             abi.encodePacked(address(this), key)
         );
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
     }
 }
